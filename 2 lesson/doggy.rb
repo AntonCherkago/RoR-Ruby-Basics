76da18f67d30344f087438b5002ee2108b25141c
AntonCherkago <antonkonovalov167@gmail.com>
2019-10-22 22:02:11 +0300

    2 commit

--- 2 lesson/doggy.rb
+++ 2 lesson/doggy.rb
@@ -1,72 +1,72 @@
 class Doggy
-  attr_accessor :name, :health, :hunger, :mood, :tiredness, :asleep, :defend
 
   def initialize(name)
+    puts 'Hello, name your pet'
+    name = gets.chomp
     @name = name
-    @health = 100
-  	@mood = 100
-  	@hunger = 0
-  	@tiredness = 0
-    @asleep = 100
+    @health = 70
+  	@mood = 70i
+  	@hunger = 30
+  	@tiredness = 30
+    @asleep = 70
     @defend = false
     puts "#{@name} начал жить с нами :)"
+    user_interaction
   end
 
-  # кормить
   def feed
 		p "Вы кормите #{@name}a"
     @hunger = 0
     @health += 10
     @mood += 10
+    @asleep -= 10
 		p "#{@name} понравилось :)"
-    timePasses
+    user_interaction
   end
 
-  # гулять
   def walk
 		p "Вы гуляете с #{@name}"
 		@tiredness += 30
     @mood += 20
-    timePasses
+    @asleep -= 15
+    if @tiredness > 90
+      puts "#{@name} wants to sleep"
+    end
+    user_interaction
   end
 
-  # спать
   def go_to_sleep
 	  p "#{@name} пошел спать"
     @hunger += 50
     @health += 25
     @asleep = 100
     @tiredness = 0
-    timePasses
-	 end
+    user_interaction
+	end
 
-   # играть
   def play
     p "Весело играть с #{@name}!"
     @mood += 30
     @tiredness += 20
     @health += 5
     @hunger += 20
-    timePasses
+    user_interaction
   end
 
-   # купаться
   def bathe
     p "Нужно привести #{@name} в порядок"
     @health += 10
     @hunger += 5
     @mood +=5
-    timePasses
+    user_interaction
   end
 
-   # гладить, почесывать
   def scratch
     p "#{@name} любит когда его почесывают за ухом"
     @mood += 20
-    timePasses
+    user_interaction
   end
 
-   # увидеть незнакомца и защитить от него
   def see_some_stranger
     p "#{@name} рычит и лает"
     @defend = true
@@ -75,50 +75,73 @@ class Doggy
     if @hunger >= 70 && @asleep <= 20
       @health -= 5
     end
-    timePasses
+    user_interaction
   end
 
-  # help
   def help
-    p "Собачку можно покормить - doggy.feed
-       С ней можно поиграть - doggy.play
-       Ее можно уложить спать - doggy.go_to_sleep
-       С ней можно погулять - doggy.walk
-       Собачку можно искупать - doggy.bathe
-       Ее можно почесать за ухом - doggy.scratch
-       Она может защитить - doggy.see_some_stranger"
+    puts 'Собачку можно покормить - feed'
+    puts 'С ней можно поиграть - play'
+    puts 'Ее можно уложить спать - go_to_sleep'
+    puts 'С ней можно погулять - walk'
+    puts 'Собачку можно искупать - bathe'
+    puts 'Ее можно почесать за ухом - scratch'
+    puts 'Она может защитить - see_some_stranger'
+    puts 'За ней можно понаблюдать - follow'
+    user_interaction
   end
 
-  # наблюдать
+  def status
+    puts "health = #{@health}"
+    puts "mood = #{@mood}"
+    puts "hunger = #{@hunger}"
+    puts "tiredness = #{@tiredness}"
+    puts "asleep = #{@asleep}"
+    puts "defend = #{@defend}"
+    user_interaction
+  end
+
   def follow
 		r = rand(1..3)
 		case r
-			when 1
-				p "#{@name} бегал по комнате, нашел мячик и схватил его :)"
-				@mood += 10
-        @tiredness += 10
-			when 2
-				p "#{@name} гулял по улице, встретил другую собаку и они облаяли друг друга"
-				@mood -= 5
-        @tiredness += 5
-			when 3
-				p "Хозяина не было целый день дома. Когда он пришел, #{@name} был очень рад и вилял хвостом"
-        @mood += 35
-        @hunger += 55
+		when 1
+			p "#{@name} бегал по комнате, нашел мячик и схватил его :)"
+		  @mood += 10
+      @tiredness += 10
+		when 2
+		  p "#{@name} гулял по улице, встретил другую собаку и они облаяли друг друга"
+			@mood -= 5
+      @tiredness += 5
+		when 3
+			p "Хозяина не было целый день дома. Когда он пришел, #{@name} был очень рад и вилял хвостом"
+      @mood += 35
+      @hunger += 35
+    end
+    user_interaction
   end
 
-   # описание метода прохождения времени
   private
 
-  def timePasses
+  def user_interaction
+    time_passes
+    puts "Выберите тип взаимодействия с вашим питомцем:"
+    action = gets.chomp
+    action_router(action)
+  end
+
+  def action_router(action)
+    send(action)
+  rescue NoMethodError
+    puts 'Неправильная команда!'
+    user_interaction
+  end
+
+  def time_passes
     @hunger += 5
-    @asleep += 5
+    @asleep -= 5
     if @hunger > 80 && @health < 20
       p "#{@name} сбегает с дома, потому что вы плохо с ним обращались("
     end
   end
-
 end
 
-
-p = Doggy.new
+doggy = Doggy.new('name')
